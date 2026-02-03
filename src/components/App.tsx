05315ccb5aa5082d8ad759e1c5a3b72968864efb
Thefinalstretch <isaccjensen02@gmail.com>
2026-02-03 23:01:08 +0100

    Fixat setup av project

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>🚀 React + Electron fungerar!</h1>
+      <p>Nu är det dags att börja med Supabase-inloggningen.</p>
+    </div>
+  );
+}
